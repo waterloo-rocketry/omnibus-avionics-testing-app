b4ea12e13deb953e66c6bd9fec1265d7fbcd4f21
Chris Yang <chrisyx511@gmail.com>
2025-10-07 18:41:42 -0400

    Added shadcn and boilerplate

--- packages/renderer/tests/App.test.tsx
+++ packages/renderer/tests/App.test.tsx
@@ -1,18 +1,19 @@
 import { describe, expect, it } from 'vitest'
 import { render, fireEvent } from '@testing-library/react'
-import App from '../src/App.tsx'
+import App from '@/App.tsx'
 
 describe('app main apge', () => {
     it('renders app component', () => {
         const component = render(<App />)
         expect(component).toBeDefined()
-        expect(component.getByText('Vite + React')).toBeDefined()
+        expect(component.getByText('Count is 0')).toBeDefined()
     })
     it('counter increments', () => {
         const component = render(<App />)
-        expect(component.getByRole('button')).toBeDefined()
-        expect(component.getByRole('button').textContent).toContain('0')
-        fireEvent.click(component.getByRole('button'))
-        expect(component.getByRole('button').textContent).toContain('1')
+        expect(component.getByText('Increment')).toBeDefined()
+        expect(component.getByText('Count is 0')).toBeDefined()
+        const button = component.getByText('Increment')
+        fireEvent.click(button)
+        expect(component.getByText('Count is 1')).toBeDefined()
     })
 })
--- packages/renderer/vite.config.ts
+++ packages/renderer/vite.config.ts
@@ -1,10 +1,18 @@
 /// <reference types="vitest/config" />
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), tsconfigPaths(), tailwindcss()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
     test: {
         environment: 'jsdom',
         setupFiles: './tests/setup.ts',
